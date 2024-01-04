00e024a70d552bc7231ca986c255e67406522dd3
nineefclass <155623874+nineefclass@users.noreply.github.com>
2024-01-04 19:34:49 +0700

    Add files via upload

---
+++ jadwal piket/script.js
@@ -0,0 +1,16 @@
+document.querySelector(".back-button").addEventListener("click", function() {
+  window.location.href = "../nineefclass";
+});
+const backButton = document.querySelector('.back-button');
+
+backButton.addEventListener('mouseover', function() {
+  this.style.transform = 'scale(1.1)';
+});
+
+backButton.addEventListener('mouseout', function() {
+  this.style.transform = 'scale(1.0)';
+});
+backButton.addEventListener('click', function() {
+  this.style.transform = 'scale(0.9)';
+  window.location.href = '../nineefclass';
+});
